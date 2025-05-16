bacaa50502deee3da7b383df282708adb86fc4d8
Leonal Robin D <leonal2003lr10@gmail.com>
2025-05-16 20:32:12 +0530

    leo

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     open: true,
     host: true
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
